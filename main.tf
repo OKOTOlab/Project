41059a2b0b93bed1cb9ce637930c393fd179b4d9
OKOTOlab <87175077+OKOTOlab@users.noreply.github.com>
2021-07-09 16:31:46 +0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "EC2-new" {
+  ami = "ami-00f22f6155d6d92c5"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_security.id]
+  key_name = "Test key pair"
+  tags = {
+    Name = "EC2-Terraform"
+  }
+  user_data = <<-EOF
+                    #!/bin/bash
+                    sudo yum install ec2-instance-connect
+                    sudo yum update -y
+                     sudo amazon-linux-extras install nginx1 -y
+                    sudo service nginx start
+                EOF
+}
+
+
+resource "aws_security_group" "web_security" {
+  name = "web-security"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "bucket_prefix" {
+  bucket_prefix = "terralab"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = 30
+    }
+
+  }
+}
+
